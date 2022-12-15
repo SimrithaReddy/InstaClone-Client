253d309f79b8fec855f2a60f33ce11cc64531fce
SimrithaReddy <simrithareddyk04@gmail.com>
2022-12-15 18:06:24 +0530

    first commit

---
+++ src/postview/card.jsx
@@ -0,0 +1,35 @@
+import "./style.css"
+
+
+const CardInstaClone = (props) => {
+    const { rec } = props
+   
+
+    return (
+        <>
+            <div className="container">
+                <section className="container-row">
+                    <article className="row container-row1">
+                        <h2 className="row bold">{props.rec.name}</h2>
+                        <p className="row light">{rec.location}</p>
+                    </article>
+                    <img src='dots-loading.png' className="dot-img" alt="toview" height='30px' width='40px'></img>
+                </section>
+
+                <img src={rec.PostImage} alt="insta-img"  height="199px" width="445px" className="img1 "></img>
+
+                <div className="icon">
+                    <img src="https://cdn-icons-png.flaticon.com/128/8184/8184268.png" alt="image1" className="icon-img" height="36px"></img>
+                    <img src="https://as2.ftcdn.net/v2/jpg/02/74/64/11/1000_F_274641162_RIC3HZnzMRILg62eQj2gfUs3xNa7Prxh.jpg" alt="image2" height="38px" className="icon-img"></img>
+
+                  
+                </div>
+                <section className="likes">{`${rec.likes} likes`}</section>
+                <section className="row">
+                    <h4 className="row-last">{rec.description}</h4>
+                </section>
+            </div>
+        </>
+    )
+}
+export default CardInstaClone;
---
+++ src/postview/uploadpost.js
@@ -0,0 +1,56 @@
+
+import { useNavigate } from "react-router-dom"
+import Header from "./header";
+import "./upload.css";
+
+const PostUpload = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let data = e.target
+    let formData = new FormData(data)
+    await fetch('https://hola-bz1b.onrender.com/uploads',
+      {
+        method: 'POST',
+        body: formData
+      })
+      .then(res => res.json()).then((d) => {
+        console.log(formData);
+        alert("post successful")
+        console.log(d);
+      }).catch((e) => { console.log(e.message) })
+      .finally(() => {
+        navigate("/InstaClone")
+      })
+  }
+
+  return (
+    <>
+    <Header></Header>
+    <hr></hr>
+      <div className="upload-container">
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <section>
+            <input type="file"  placeholder="file" id="post" name="PostImage"></input>
+          </section>
+
+          <section>
+            <input type="text" required placeholder="Author" name="name"></input>
+          </section>
+
+          <section>
+            <input type="text" required placeholder="Location" name="location"></input>
+          </section>
+
+          <section>
+            <input type="text" required placeholder="Description" name="description"></input>
+          </section>
+          <button type="submit">Post</button>
+        </form>
+      </div>
+    </>
+  );
+}
+export default PostUpload;
